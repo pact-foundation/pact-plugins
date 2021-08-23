003d0c4e0903c3cabece6b61adc998e9057fee38
Ronald Holshausen <rholshausen@dius.com.au>
2021-08-23 14:52:11 +1000

    chore: to publish the rust driver, the proto file needs to be included

--- drivers/rust/driver/build.rs
+++ drivers/rust/driver/build.rs
@@ -3,7 +3,7 @@ use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   let proto_file = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?)
-    .join("../../../proto/plugin.proto").canonicalize()?;
+    .join("plugin.proto").canonicalize()?;
   tonic_build::compile_protos(proto_file.to_string_lossy().to_string())?;
   Ok(())
 }
