8bfd42dc54f2a018be81216cb12bcafc207403d7
Ronald Holshausen <rholshausen@dius.com.au>
2021-08-23 14:47:45 +1000

    chore: set proto dir relative to cargo manifest dir

--- drivers/rust/driver/build.rs
+++ drivers/rust/driver/build.rs
@@ -1,4 +1,9 @@
+use std::path::PathBuf;
+use std::env;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  tonic_build::compile_protos("../../../proto/plugin.proto")?;
+  let proto_file = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?)
+    .join("../../../proto/plugin.proto").canonicalize()?;
+  tonic_build::compile_protos(proto_file.to_string_lossy().to_string())?;
   Ok(())
 }
