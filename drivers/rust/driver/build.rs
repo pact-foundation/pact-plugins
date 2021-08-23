d10f41e75917b12af51a285cbced8af2ef809052
Ronald Holshausen <rholshausen@dius.com.au>
2021-08-23 15:39:28 +1000

    chore: correct build script for windows

--- drivers/rust/driver/build.rs
+++ drivers/rust/driver/build.rs
@@ -1,7 +1,4 @@
-use std::path::PathBuf;
-use std::env;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  tonic_build::compile_protos("plugin.proto")?;
+  tonic_build::compile_protos("./plugin.proto")?;
   Ok(())
 }
