4361ed686fa9f2e79d5ca107c0474e7661b61cfb
Ronald Holshausen <rholshausen@dius.com.au>
2021-09-02 11:39:43 +1000

    feat: add simple example Go provider

---
+++ examples/protobuf/protobuf-provider/go.mod
@@ -0,0 +1,8 @@
+module io.pact.plugins.protobuf/provider
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/protobuf v1.27.1
+)
---
+++ examples/protobuf/protobuf-provider/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "log"
+import "net/http"
+import "github.com/golang/protobuf/proto"
+import "io.pact.plugins.protobuf/provider/io.pact.plugin"
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+      w.Header().Add("Content-Type", "application/protobuf; message=InitPluginRequest")
+      init := &io_pact_plugin.InitPluginRequest{
+              Implementation: "Go Provider",
+//               Version:  "000",
+      }
+
+      out, err := proto.Marshal(init)
+      if err != nil {
+              log.Fatalln("Failed to encode address init request:", err)
+      } else {
+        w.Write(out)
+      }
+    })
+
+    log.Fatal(http.ListenAndServe("localhost:8111", mux))
+}
