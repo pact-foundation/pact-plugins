58cbeaaa22248e62963cd7e1585276a03e010ba0
Ronald Holshausen <rholshausen@dius.com.au>
2021-09-10 16:47:39 +1000

    feat: add a synchronized guard to loadPlugin

--- drivers/jvm/core/src/main/kotlin/io/pact/plugins/jvm/core/PluginManager.kt
+++ drivers/jvm/core/src/main/kotlin/io/pact/plugins/jvm/core/PluginManager.kt
@@ -205,17 +205,18 @@ object DefaultPluginManager: KLogging(), PluginManager {
   }
 
   override fun loadPlugin(name: String, version: String?): Result<PactPlugin, String> {
-    val plugin = lookupPlugin(name, version)
-    return if (plugin != null) {
-      PluginMetrics.sendMetrics(plugin.manifest)
-      Ok(plugin)
-    } else {
-      when (val manifest = loadPluginManifest(name, version)) {
-        is Ok -> {
-          PluginMetrics.sendMetrics(manifest.value)
-          initialisePlugin(manifest.value)
+    synchronized(PLUGIN_REGISTER) {
+      val plugin = lookupPlugin(name, version)
+      return if (plugin != null) {
+        Ok(plugin)
+      } else {
+        when (val manifest = loadPluginManifest(name, version)) {
+          is Ok -> {
+            PluginMetrics.sendMetrics(manifest.value)
+            initialisePlugin(manifest.value)
+          }
+          is Err -> Err(manifest.error)
         }
-        is Err -> Err(manifest.error)
       }
     }
   }
