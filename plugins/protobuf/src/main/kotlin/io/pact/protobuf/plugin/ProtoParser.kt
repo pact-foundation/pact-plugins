34197471f4286b95f44e9470ba7edb5ea6186997
Ronald Holshausen <ronald.holshausen@gmail.com>
2021-10-13 14:55:55 +1100

    fix: system protoc needs the well known types - unpacked from protobuf jar

--- plugins/protobuf/src/main/kotlin/io/pact/protobuf/plugin/ProtoParser.kt
+++ plugins/protobuf/src/main/kotlin/io/pact/protobuf/plugin/ProtoParser.kt
@@ -7,16 +7,19 @@ import com.google.protobuf.DescriptorProtos
 import io.pact.plugins.jvm.core.SystemExec
 import io.pact.plugins.jvm.core.Utils
 import mu.KLogging
+import java.io.File
 import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.path.deleteIfExists
 
 object ProtoParser : KLogging() {
   fun parseProtoFile(protoFile: Path): DescriptorProtos.FileDescriptorSet {
     logger.debug { "Parsing protobuf proto file $protoFile" }
 
-    val tempdir = Files.createTempDirectory(null)
-    val outFile = Files.createTempFile(tempdir, null, null)
+    val tmpDir = Path.of("./tmp/")
+    tmpDir.toFile().mkdirs()
+    val outFile = Files.createTempFile(tmpDir, null, null)
 
     val args = arrayOf("--include_std_types", "-o${outFile}", "-I${protoFile.parent}", "--include_imports", protoFile.toString())
     logger.debug { "Invoking bundled protoc with ${args.toList()}" }
@@ -24,8 +27,9 @@ object ProtoParser : KLogging() {
       logger.error { "Failed to run the bundled protoc, will try the system one" }
       when (val protocPath = Utils.lookForProgramInPath("protoc")) {
         is Ok -> {
-          val systemArgs = args.drop(1).toTypedArray()
-          logger.debug { "Executing: ${protocPath.value} $systemArgs" }
+          unpackWellKnownTypes(tmpDir)
+          val systemArgs = args.drop(1).plus("-I./tmp").toTypedArray()
+          logger.debug { "Executing: ${protocPath.value} ${systemArgs.toList()}" }
           when (val result = SystemExec.execute(protocPath.value.toString(), *systemArgs)) {
             is Ok -> {}
             is Err -> throw RuntimeException("Failed to execute protoc in the system path - ${result.error.first} ${result.error.second}")
@@ -35,7 +39,32 @@ object ProtoParser : KLogging() {
       }
     }
 
-    return Files.newInputStream(outFile).use { DescriptorProtos.FileDescriptorSet.parseFrom(it) }
+    val fileDescriptorSet = Files.newInputStream(outFile).use { DescriptorProtos.FileDescriptorSet.parseFrom(it) }
+    outFile.deleteIfExists()
+    return fileDescriptorSet
+  }
+
+  private fun unpackWellKnownTypes(tmpDir: Path) {
+    val includeDir = File(tmpDir.toFile(), "google/protobuf")
+    includeDir.mkdirs()
+    writeFile(includeDir, "struct.proto")
+    writeFile(includeDir, "any.proto")
+    writeFile(includeDir, "api.proto")
+    writeFile(includeDir, "descriptor.proto")
+    writeFile(includeDir, "duration.proto")
+    writeFile(includeDir, "empty.proto")
+    writeFile(includeDir, "field_mask.proto")
+    writeFile(includeDir, "source_context.proto")
+    writeFile(includeDir, "struct.proto")
+    writeFile(includeDir, "timestamp.proto")
+    writeFile(includeDir, "type.proto")
+    writeFile(includeDir, "wrappers.proto")
+  }
+
+  private fun writeFile(includeDir: File, name: String) {
+    File(includeDir, name).printWriter().use {
+      it.write(javaClass.getResource("/google/protobuf/$name").readText())
+    }
   }
 
   private fun runProtoc(args: Array<String>): Boolean {
