0e1d430b5d5eca813e6f6827c576e39d901ed10f
Ronald Holshausen <ronald.holshausen@pactflow.io>
2021-12-10 11:28:27 +1100

    fix: Correct the plugin load metric event

--- drivers/jvm/core/src/main/kotlin/io/pact/plugins/jvm/core/PluginMetrics.kt
+++ drivers/jvm/core/src/main/kotlin/io/pact/plugins/jvm/core/PluginMetrics.kt
@@ -1,15 +1,20 @@
 package io.pact.plugins.jvm.core
 
-import au.com.dius.pact.core.support.Json
+import au.com.dius.pact.core.support.contains
 import au.com.dius.pact.core.support.isNotEmpty
 import io.pact.plugins.jvm.core.Utils.lookupVersion
 import mu.KLogging
-import org.apache.hc.client5.http.fluent.Request.post
-import org.apache.hc.core5.http.ContentType
-import org.apache.hc.core5.http.io.entity.StringEntity
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.hc.client5.http.fluent.Request
+import org.apache.hc.core5.http.message.BasicNameValuePair
 import java.util.UUID
+import java.util.concurrent.TimeUnit
 
 object PluginMetrics: KLogging() {
+
+  const val GA_ID = "UA-117778936-1"
+  const val GA_URL = "https://www.google-analytics.com/collect"
+
   fun sendMetrics(manifest: PactPluginManifest) {
     Thread {
       val doNotTrack = lookupProperty("pact_do_not_track").ifNullOrEmpty {
@@ -23,29 +28,35 @@ object PluginMetrics: KLogging() {
           """
         }
         try {
-          val osArch = "${lookupProperty("os.name")?.lowercase()}-${lookupProperty("os.arch")?.lowercase()}"
+          val osName = lookupProperty("os.name")?.lowercase().orEmpty()
+          val osArch = "$osName-${lookupProperty("os.arch")?.lowercase()}"
           val entity = mapOf(
-            "v" to 1,                                         // Version of the API
-            "tid" to "UA-117778936-1",                        // Property ID
-            "cid" to UUID.randomUUID().toString(),            // Anonymous Client ID.
-            "an" to "pact-plugins-jvm",                       // App name.
-            "aid" to "pact-plugins-jvm",                      // App Id
-            "av" to lookupVersion(PluginMetrics::class.java), // App version.
-            "aip" to true,                                    // Anonymise IP address
-            "ds" to "pact-plugins-jvm",                       // Data source
-            "cd1" to "pact-plugins-jvm",                      // Custom Dimension 1: library
-            "cd2" to lookupContext(),                         // Custom Dimension 2: context
-            "cd3" to osArch,                                  // Custom Dimension 3: osarch
-            "cd4" to manifest.name,                           // Custom Dimension 4: plugin_name
-            "cd5" to manifest.version,                        // Custom Dimension 5: plugin_version
-            "el" to "Plugin loaded",                          // Event
-            "ec" to "Plugin",                                 // Category
-            "ea" to "Loaded",                                 // Action
-            "ev" to 1                                         // Value
+            "v" to 1,                                               // Version of the API
+            "t" to "event",                                         // Hit type, Specifies the metric is for an event
+            "tid" to GA_ID,                                         // Property ID
+            "uid" to hostnameHash(osName),                          // Anonymous Client ID.
+            "an" to "pact-plugins-jvm",                             // App name.
+            "aid" to "pact-plugins-jvm",                            // App Id
+            "av" to lookupVersion(PluginMetrics::class.java),       // App version.
+            "aip" to true,                                          // Anonymise IP address
+            "ds" to "client",                                       // Data source
+            "cd2" to lookupContext(),                               // Custom Dimension 2: context
+            "cd3" to osArch,                                        // Custom Dimension 3: osarch
+            "cd4" to manifest.name,                                 // Custom Dimension 4: plugin_name
+            "cd5" to manifest.version,                              // Custom Dimension 5: plugin_version
+            "cd7" to lookupProperty("java.runtime.version"),  // Custom Dimension 7: platform_version
+            "el" to "Plugin loaded",                                // Event
+            "ec" to "Plugin",                                       // Category
+            "ea" to "Loaded",                                       // Action
+            "ev" to 1                                               // Value
           )
-          val stringEntity = StringEntity(Json.toJson(entity).serialise(), ContentType.APPLICATION_JSON)
-          val response = post("https://www.google-analytics.com/collect")
-            .body(stringEntity)
+            .filterValues { it != null }
+            .map {
+              BasicNameValuePair(it.key, it.value.toString())
+            }
+
+          val response = Request.post(GA_URL)
+            .bodyForm(entity)
             .execute()
             .returnResponse()
           if (response.code > 299) {
@@ -55,7 +66,34 @@ object PluginMetrics: KLogging() {
           logger.debug(ex) { "Failed to send plugin load metrics" }
         }
       }
-    }.run()
+    }.start()
+  }
+
+  /**
+   * This function makes a MD5 hash of the hostname
+   */
+  private fun hostnameHash(osName: String): String {
+    val hostName = if (osName.contains("windows")) {
+      lookupEnv("COMPUTERNAME")
+    } else {
+      lookupEnv("HOSTNAME")
+    }
+    val hashData = hostName
+      .ifNullOrEmpty { execHostnameCommand() }
+      .ifNullOrEmpty { UUID.randomUUID().toString() }
+
+    return DigestUtils(DigestUtils.getMd5Digest()).digestAsHex(hashData!!.toByteArray())
+  }
+
+  private fun execHostnameCommand(): String? {
+    val pb = ProcessBuilder("hostname").start()
+    pb.waitFor(500, TimeUnit.SECONDS)
+    return if (pb.exitValue() == 0) {
+      pb.inputStream.bufferedReader().readLine().trim()
+    } else {
+      // Host name process failed
+      null
+    }
   }
 
   private fun lookupProperty(name: String): String? = System.getProperty(name)
