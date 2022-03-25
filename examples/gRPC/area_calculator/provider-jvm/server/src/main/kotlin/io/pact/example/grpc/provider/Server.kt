983727e9fa294214e9d944c8702e88b2542ea76d
Ronald Holshausen <rholshausen@dius.com.au>
2022-03-25 14:02:41 +1100

    chore: added basic calculator server

--- examples/gRPC/area_calculator/provider-jvm/server/src/main/kotlin/io/pact/example/grpc/provider/Server.kt
+++ examples/gRPC/area_calculator/provider-jvm/server/src/main/kotlin/io/pact/example/grpc/provider/Server.kt
@@ -1,12 +1,38 @@
 package io.pact.example.grpc.provider
 
+import area_calculator.CalculatorGrpcKt
+import io.grpc.ServerBuilder
+
 class Server {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
+    private val server = ServerBuilder.forPort(0).addService(CalculatorService()).build()
+
+    fun start() {
+        server.start()
+        println("Started calculator service on ${server.port}")
+        Runtime.getRuntime().addShutdownHook(
+            Thread {
+                println("*** shutting down gRPC server since JVM is shutting down")
+                server.shutdownNow()
+                println("*** server shut down")
+            }
+        )
+    }
+
+    fun stop() {
+        server.shutdown()
+    }
+
+    fun blockUntilShutdown() {
+        server.awaitTermination()
+    }
+}
+
+class CalculatorService : CalculatorGrpcKt.CalculatorCoroutineImplBase() {
+
 }
 
 fun main() {
-    println(Server().greeting)
+    val server = Server()
+    server.start()
+    server.blockUntilShutdown()
 }
