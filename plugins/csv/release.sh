0cc41be5e4f19334694086818a90e588f30fe882
Ronald Holshausen <rholshausen@dius.com.au>
2022-05-02 16:27:02 +1000

    chore: setup automated release for CSV plugin

---
+++ plugins/csv/release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+    echo "Usage : $0 <Linux|Windows|macOS>"
+    exit
+fi
+
+echo Building Release for "$1"
+
+cargo clean
+mkdir -p /tmp/artifacts/
+cargo build --release
+
+case "$1" in
+  Linux)    echo "Building for Linux"
+            gzip -c target/release/pact-protobuf-plugin > /tmp/artifacts/pact-protobuf-plugin-linux-x86_64.gz
+            openssl dgst -sha256 -r /tmp/artifacts/pact-protobuf-plugin-linux-x86_64.gz > /tmp/artifacts/pact-protobuf-plugin-linux-x86_64.gz.sha256
+            cp pact-plugin.json /tmp/artifacts/
+            ;;
+  Windows)  echo  "Building for Windows"
+            gzip -c target/release/pact-protobuf-plugin.exe > /tmp/artifacts/pact-protobuf-plugin-windows-x86_64.exe.gz
+            openssl dgst -sha256 -r /tmp/artifacts/pact-protobuf-plugin-windows-x86_64.exe.gz > /tmp/artifacts/pact-protobuf-plugin-windows-x86_64.exe.gz.sha256
+            ;;
+  macOS)    echo  "Building for OSX"
+            gzip -c target/release/pact-protobuf-plugin > /tmp/artifacts/pact-protobuf-plugin-osx-x86_64.gz
+            openssl dgst -sha256 -r /tmp/artifacts/pact-protobuf-plugin-osx-x86_64.gz > /tmp/artifacts/pact-protobuf-plugin-osx-x86_64.gz.sha256
+
+            # M1
+            export SDKROOT=$(xcrun -sdk macosx11.1 --show-sdk-path)
+            export MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.1 --show-sdk-platform-version)
+            cargo build --target aarch64-apple-darwin --release
+
+            gzip -c target/aarch64-apple-darwin/release/pact-protobuf-plugin > /tmp/artifacts/pact-protobuf-plugin-osx-aarch64.gz
+            openssl dgst -sha256 -r /tmp/artifacts/pact-protobuf-plugin-osx-aarch64.gz > /tmp/artifacts/pact-protobuf-plugin-osx-aarch64.gz.sha256
+            ;;
+  *)        echo "$1 is not a recognised OS"
+            exit 1
+            ;;
+esac
