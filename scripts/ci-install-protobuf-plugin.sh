78e7b6bfd702cd0eebeca50e57f4b42332320f24
Ronald Holshausen <ronald.holshausen@smartbear.com>
2022-05-24 11:32:33 +1000

    chore(CI): install the grpc plugin instead of prototype one

--- scripts/ci-install-protobuf-plugin.sh
+++ scripts/ci-install-protobuf-plugin.sh
@@ -3,18 +3,18 @@
 set -e
 
 LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/pactflow/pact-protobuf-plugin/releases/latest | jq -r '.tag_name')
-curl -o /tmp/install-plugin.sh https://github.com/pactflow/pact-protobuf-plugin/releases/download/"${LATEST_RELEASE}"/install-plugin.sh
-chmod +x /tmp/install-plugin.sh
 
 case "$(uname -s)" in
 
    CYGWIN*|MINGW32*|MSYS*|MINGW*)
-     export PATH=$PATH:/mnt/C/:\msys64\usr\bin
+     export PATH=$PATH:/c/msys64/usr/bin
      ;;
 
    *)
      ;;
 esac
 
+wget -o /tmp/install-plugin.sh https://github.com/pactflow/pact-protobuf-plugin/releases/download/"${LATEST_RELEASE}"/install-plugin.sh
+chmod +x /tmp/install-plugin.sh
 
 /tmp/install-plugin.sh
