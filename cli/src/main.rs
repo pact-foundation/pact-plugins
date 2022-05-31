aef7d3651ae7271cd58180e081e49f3e4f357749
Ronald Holshausen <ronald.holshausen@smartbear.com>
2022-05-31 11:07:20 +1000

    feat: added env config command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,7 @@
+use std::env;
 use clap::{Parser, Subcommand};
+use comfy_table::presets::UTF8_FULL;
+use comfy_table::Table;
 
 #[derive(Parser, Debug)]
 #[clap(version, about)]
@@ -34,10 +37,40 @@ fn main() {
 
   match &cli.command {
     Commands::List => {}
-    Commands::Env => {}
+    Commands::Env => print_env(),
     Commands::Install => {}
     Commands::Remove => {}
     Commands::Enable => {}
     Commands::Disable => {}
+  }
+}
+
+fn print_env() {
+  let mut table = Table::new();
+
+  let (plugin_src, plugin_dir) = resolve_plugin_dir();
+
+  table
+    .load_preset(UTF8_FULL)
+    .set_header(vec!["Configuration", "Source", "Value"])
+    .add_row(vec!["Plugin Directory", plugin_src.as_str(), plugin_dir.as_str()]);
+
+  println!("{table}");
+}
+
+fn resolve_plugin_dir() -> (String, String) {
+  let home_dir = home::home_dir()
+    .map(|dir| dir.join(".pact/plugins"))
+    .unwrap_or_default();
+  match env::var_os("PACT_PLUGIN_DIR") {
+    None => ("$HOME/.pact/plugins".to_string(), home_dir.display().to_string()),
+    Some(dir) => {
+      let plugin_dir = dir.to_string_lossy();
+      if plugin_dir.is_empty() {
+        ("$HOME/.pact/plugins".to_string(), home_dir.display().to_string())
+      } else {
+        ("$PACT_PLUGIN_DIR".to_string(), plugin_dir.to_string())
+      }
+    }
   }
 }
