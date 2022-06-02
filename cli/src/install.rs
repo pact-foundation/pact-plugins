8b0517fc4dd148b4aee414116508402aa7a3fedd
Ronald Holshausen <ronald.holshausen@smartbear.com>
2022-06-02 16:44:53 +1000

    fix: used wrong function

--- cli/src/install.rs
+++ cli/src/install.rs
@@ -128,7 +128,8 @@ fn gunzip_file(
   let file = if ext.is_empty() {
     plugin_dir.join(&manifest.entry_point)
   } else {
-    plugin_dir.join(&manifest.entry_point).with_extension(ext.strip_prefix('.'))
+    plugin_dir.join(&manifest.entry_point)
+      .with_extension(ext.strip_prefix('.').unwrap_or(ext))
   };
   let mut f = File::create(file.clone())?;
   let mut gz = GzDecoder::new(File::open(gz_file)?);
