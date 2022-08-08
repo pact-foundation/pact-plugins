13dbf90ee05264db15d187df9e9be10b5c26c486
Ronald Holshausen <ronald.holshausen@gmail.com>
2022-08-08 17:21:17 +1000

    feat: Build the CVS plugin with Alpine

--- plugins/csv/release.sh
+++ plugins/csv/release.sh
@@ -10,19 +10,21 @@ echo Building Release for "$1"
 
 cargo clean
 mkdir -p target/artifacts/
-cargo build --release
 
 case "$1" in
   Linux)    echo "Building for Linux"
+            docker run --rm --user "$(id -u)":"$(id -g)" -v "$(pwd):/workspace" -w /workspace -t pactfoundation/rust-musl-build -c 'cargo build --release'
             gzip -c target/release/pact-csv-plugin > target/artifacts/pact-csv-plugin-linux-x86_64.gz
             openssl dgst -sha256 -r target/artifacts/pact-csv-plugin-linux-x86_64.gz > target/artifacts/pact-csv-plugin-linux-x86_64.gz.sha256
             cp pact-plugin.json target/artifacts/
             ;;
   Windows)  echo  "Building for Windows"
+            cargo build --release
             gzip -c target/release/pact-csv-plugin.exe > target/artifacts/pact-csv-plugin-windows-x86_64.exe.gz
             openssl dgst -sha256 -r target/artifacts/pact-csv-plugin-windows-x86_64.exe.gz > target/artifacts/pact-csv-plugin-windows-x86_64.exe.gz.sha256
             ;;
   macOS)    echo  "Building for OSX"
+            cargo build --release
             gzip -c target/release/pact-csv-plugin > target/artifacts/pact-csv-plugin-osx-x86_64.gz
             openssl dgst -sha256 -r target/artifacts/pact-csv-plugin-osx-x86_64.gz > target/artifacts/pact-csv-plugin-osx-x86_64.gz.sha256
 
