43e985c7840d4132b4eaf535053aa4e62f9eca3d
Ronald Holshausen <ronald.holshausen@gmail.com>
2022-08-08 13:24:15 +1000

    chore: use pact-plugin-cli to install the protobuf plugin

--- scripts/ci-install-protobuf-plugin.sh
+++ scripts/ci-install-protobuf-plugin.sh
@@ -2,19 +2,5 @@
 
 set -e
 
-LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/pactflow/pact-protobuf-plugin/releases/latest | jq -r '.tag_name')
-
-case "$(uname -s)" in
-
-   CYGWIN*|MINGW32*|MSYS*|MINGW*)
-     export PATH=$PATH:/c/msys64/usr/bin
-     ;;
-
-   *)
-     ;;
-esac
-
-mkdir -p ~/.pact/plugins
-wget https://github.com/pactflow/pact-protobuf-plugin/releases/download/"${LATEST_RELEASE}"/install-plugin.sh -O ~/.pact/plugins/install-plugin.sh
-chmod +x ~/.pact/plugins/install-plugin.sh
-~/.pact/plugins/install-plugin.sh
+cargo install pact-plugin-cli
+pact-plugin-cli install https://github.com/pactflow/pact-protobuf-plugin/releases/latest
