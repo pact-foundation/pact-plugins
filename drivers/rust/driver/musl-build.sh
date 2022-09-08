a54765f8850e1d200869a164344c0bedcc97b8d3
Ronald Holshausen <ronald.holshausen@gmail.com>
2022-09-08 15:11:14 +1000

    chore: run build with latest CSV plugin

--- drivers/rust/driver/musl-build.sh
+++ drivers/rust/driver/musl-build.sh
@@ -5,6 +5,6 @@ set -e
 apk add protobuf protobuf-dev
 cargo install pact-plugin-cli
 pact-plugin-cli -y install https://github.com/pactflow/pact-protobuf-plugin/releases/latest
-# pact-plugin-cli install https://github.com/pact-foundation/pact-plugins/releases/tag/csv-plugin-0.0.2
+# pact-plugin-cli install https://github.com/pact-foundation/pact-plugins/releases/tag/csv-plugin-0.0.3
 cargo build
 cargo test
