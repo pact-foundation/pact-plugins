8dd71a737f74f2ca7545573f00fd4dbd9fc73b70
Ronald Holshausen <ronald.holshausen@gmail.com>
2022-09-12 15:05:30 +1000

    chore: more debug Go provider CI build

---
+++ scripts/run-grpc-examples.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+cd consumer-jvm
+./gradlew check
+
+cd ../consumer-rust
+pact_do_not_track=true cargo test
+
+cd ../consumer-go
+go test -c
+ls -la
+pact_do_not_track=true ./consumer.test
+
+cd ../provider-jvm
+cp ../consumer-jvm/build/pacts/* server/src/test/resources/pacts
+cp ../consumer-rust/target/pacts/* server/src/test/resources/pacts
+cp ../consumer-go/pacts/* server/src/test/resources/pacts
+./gradlew check
+
+cd ../provider-go
+go build provider.go
+nohup ./provider > provider.go.out 2>&1 &
+PID=$!
+trap "kill $PID" EXIT
+sleep 1
+ls -la
+PROVIDER_PORT=$(cat provider.go.out | cut -f4 -d:)
+pact_do_not_track=true ~/bin/pact_verifier_cli -f ../consumer-jvm/build/pacts/grpc-consumer-jvm-area-calculator-provider.json\
+  -f ../consumer-rust/target/pacts/grpc-consumer-rust-area-calculator-provider.json\
+  -f ../consumer-go/pacts/grpc-consumer-go-area-calculator-provider.json\
+  -p "$PROVIDER_PORT"
