cf1d8290847a0b52cf4ebc54bf342c3c9e2a9521
Ronald Holshausen <ronald.holshausen@smartbear.com>
2023-02-08 14:47:00 +1100

    feat: add consumer example that uses gRPC metadata

---
+++ examples/gRPC/metadata/consumer-jvm/src/test/java/io/pact/example/grpc/consumer/PactConsumerTest.java
@@ -0,0 +1,74 @@
+package io.pact.example.grpc.consumer;
+
+import au.com.dius.pact.consumer.MockServer;
+import au.com.dius.pact.consumer.dsl.PactBuilder;
+import au.com.dius.pact.consumer.junit.MockServerConfig;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.consumer.junit5.ProviderType;
+import au.com.dius.pact.consumer.model.MockServerImplementation;
+import au.com.dius.pact.core.model.PactSpecVersion;
+import au.com.dius.pact.core.model.V4Interaction;
+import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import com.google.protobuf.InvalidProtocolBufferException;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.MetadataUtils;
+import metadatatest.Metadata;
+import metadatatest.TestGrpc;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.Map;
+
+import static au.com.dius.pact.consumer.dsl.PactBuilder.filePath;
+import static io.grpc.Metadata.ASCII_STRING_MARSHALLER;
+import static metadatatest.TestGrpc.newBlockingStub;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "validate-token-provider", providerType = ProviderType.SYNCH_MESSAGE, pactVersion = PactSpecVersion.V4)
+public class PactConsumerTest {
+  @Pact(consumer = "grpc-consumer-jvm")
+  V4Pact testMetadataPact(PactBuilder builder) {
+    return builder
+      .usingPlugin("protobuf")
+      .expectsToReceive("validate token request", "core/interaction/synchronous-message")
+      .with(Map.of(
+        "pact:proto", filePath("../proto/metadata.proto"),
+        "pact:content-type", "application/grpc",
+        "pact:proto-service", "Test/ValidateToken",
+
+        "request", Map.of(),
+        "requestMetadata", Map.of(
+          "Auth", "matching(regex, '[A-Z]{3}\\d+', 'AST00004')"
+        ),
+
+        "response", Map.of(
+          "ok", "matching(boolean, true)"
+        ),
+        "responseMetadata", Map.of(
+          "code", "matching(integer, 100)"
+        )
+      ))
+      .toPact();
+  }
+
+  @Test
+  @MockServerConfig(implementation = MockServerImplementation.Plugin, registryEntry = "protobuf/transport/grpc")
+  void testMetadata(MockServer mockServer, V4Interaction.SynchronousMessages interaction) throws InvalidProtocolBufferException {
+    io.grpc.Metadata metadata = new io.grpc.Metadata();
+    metadata.put(io.grpc.Metadata.Key.of("Auth", ASCII_STRING_MARSHALLER), "AB123");
+    ManagedChannel channel = ManagedChannelBuilder.forTarget("127.0.0.1:" + mockServer.getPort())
+      .usePlaintext()
+      .intercept(MetadataUtils.newAttachHeadersInterceptor(metadata))
+      .build();
+    TestGrpc.TestBlockingStub stub = newBlockingStub(channel);
+
+    Metadata.ValidateTokenRequest request = Metadata.ValidateTokenRequest.newBuilder().build();
+    Metadata.ValidateTokenResult response = stub.validateToken(request);
+    assertThat(response.getOk(), equalTo(true));
+  }
+}
