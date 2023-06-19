949859ed873757afc9ab851b172a781d7c7ac749
Ronald Holshausen <ronald.holshausen@smartbear.com>
2023-06-19 15:55:01 +1000

    chore: Add V2 plugin proto file

--- drivers/rust/driver/build.rs
+++ drivers/rust/driver/build.rs
@@ -2,7 +2,8 @@ use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   if env::var_os("PACT_PLUGIN_BUILD_PROTOBUFS").is_some() {
-    tonic_build::compile_protos("./plugin.proto")?;
+    tonic_build::compile_protos("../../../proto/plugin.proto")?;
+    tonic_build::compile_protos("../../../proto/plugin_v2.proto")?;
   }
   Ok(())
 }
