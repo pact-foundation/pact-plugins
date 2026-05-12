90c2c17ddddaf3471170c5155b98bf630825a942
Ronald Holshausen <ronald.holshausen@smartbear.com>
2026-05-12 15:56:30 +1000

    chore(rust-driver): Upgrade Prost to 0.14.3 and Tonic to 0.14.6

--- drivers/rust/driver/build.rs
+++ drivers/rust/driver/build.rs
@@ -1,8 +1,10 @@
 use std::env;
 
+use tonic_prost_build::compile_protos;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   if env::var_os("PACT_PLUGIN_BUILD_PROTOBUFS").is_some() {
-    tonic_build::compile_protos("./plugin.proto")?;
+    compile_protos("./plugin.proto")?
   }
   Ok(())
 }
